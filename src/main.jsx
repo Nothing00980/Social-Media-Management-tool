6b34d896e7693370d5ac50da3485581b510af9cd
Nothing00980 <yuvrajbhati00980@gmail.com>
2025-05-15 00:09:01 +0530

    final/clerkadded

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { ClerkProvider } from "@clerk/clerk-react";
-import { BrowserRouter } from "react-router-dom";
 import App from './App.jsx'
 import './index.css'
 
 
-// const clerkFrontendApi = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 createRoot(document.getElementById('root')).render(
   <StrictMode>
         <App />
